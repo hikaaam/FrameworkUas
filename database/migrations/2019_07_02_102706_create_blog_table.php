a856e4bb68f6bd40216ef15db06761166b28fc6b
Fredy Nur Apriyanto <mail.fredyna@gmail.com>
2019-07-02 17:29:54 +0700

    table blog

---
+++ database/migrations/2019_07_02_102706_create_blog_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBlogTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('blog_tbl', function (Blueprint $table) {
+            Schema::create('blog_tbl', function (Blueprint $table) {
+                $table->bigIncrements('id')->unique();
+                $table->string('judul', 50);
+                $table->string('isi_berita');
+                $table->string('foto')->nullable();
+                $table->timestamps();
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog_tbl');
+    }
+}
